85172b3c0b0d7a87edfe1456c5d7edaac68b2e0d
goenning <me@goenning.net>
2024-10-06 08:42:57 +0100

    bootstrap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://kubespec.dev',
+  integrations: [tailwind(), sitemap()]
+});
