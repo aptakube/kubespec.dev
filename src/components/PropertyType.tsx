da700f5a4334e2dd99a79ade768b26e3b871ae4e
goenning <me@goenning.net>
2024-10-10 20:28:39 +0100

    more pages

--- src/components/PropertyType.tsx
+++ src/components/PropertyType.tsx
@@ -5,15 +5,15 @@ type Props = {
 
 export function PropertyType(props: Props) {
   const color = props.hasChildren
-    ? 'text-pink-600'
+    ? 'text-pink-600 dark:text-pink-400'
     : props.type === 'string'
-    ? 'text-orange-600'
+    ? 'text-orange-600 dark:text-pink-400'
     : props.type === 'boolean'
-    ? 'text-blue-600'
+    ? 'text-blue-600 dark:text-blue-400'
     : props.type === 'integer'
-    ? 'text-sky-600'
+    ? 'text-sky-600 dark:text-sky-400'
     : props.type === 'Time' || props.type === 'object'
-    ? 'text-violet-600'
-    : 'text-emerald-600'
+    ? 'text-violet-600 dark:text-violet-400'
+    : 'text-emerald-600 dark:text-emerald-400'
   return <span className={`text-xs ${color}`}>{props.type}</span>
 }
