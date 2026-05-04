f14af73074c965139fbc97ae320259c7cf1d9ffe
kkocwin-pcss <247206047+kkocwin-pcss@users.noreply.github.com>
2026-05-04 08:33:20 +0200

    feat: Add grafana and loki operators (#64)
    * feat: Add grafana-operator
    
    * feat: Add loki-operator

--- src/lib/kube/projects.ts
+++ src/lib/kube/projects.ts
@@ -196,4 +196,20 @@ export default [
     pathToManifests: ["charts/kargo/resources/crds/"],
     filterTag: (tag: string) => semver.gte(tag, "v1.10.2"),
   },
+  {
+    name: "Grafana Operator",
+    slug: "grafana-operator",
+    logo: "https://avatars.githubusercontent.com/u/7195757?s=48&v=4",
+    repo: "grafana/grafana-operator",
+    pathToManifests: ["deploy/helm/grafana-operator/files/crds"],
+    filterTag: (tag: string) => tag.startsWith("v") && semver.valid(tag) && semver.gte(tag, "v1.0.0"),
+  },
+  {
+    name: "Loki Operator",
+    slug: "loki",
+    logo: "https://avatars.githubusercontent.com/u/7195757?s=48&v=4",
+    repo: "grafana/loki",
+    pathToManifests: ["operator/bundle/community/manifests"],
+    filterTag: (tag: string) => tag.startsWith("v") && semver.valid(tag) && semver.gte(tag, "v0.1.0"),
+  },
 ];
